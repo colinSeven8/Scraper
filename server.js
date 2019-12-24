a2bf0860898ca399f9ed61d6d47a21d1014c8adc
colinSeven8 <ccnorse80@gmail.com>
2019-12-23 18:01:12 -0700

    Got sidetracked on some handlebars, worked on index and main

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 // Dependencies
-let express = require("express");
-let mongojs = require("mongojs");
-let axios = require("axios");
-let cheerio = require("cheerio");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-// Initialize Express
-let app = express();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const db = require('../../models');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// If deployed, use the deployed database. Otherwise use the local headHolelines database
+//const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/headHolelines";
+//mongoose.connect(MONGODB_URI);
 
 // Database configuration
-let databaseUrl = "scraper";
-let collections = ["scrapedData"];
+const databaseUrl = "scraper";
+const collections = ["scrapedData"];
 
 // Hook mongojs configuration to the db variable
-let db = mongojs(databaseUrl, collections);
+const db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
@@ -37,42 +47,38 @@ app.get("/all", function(req, res) {
   });
 });
 
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request via axios for the news section of `ycombinator`
-  axios.get("https://www.clickhole.com/").then(function(response) {
-    // Load the html body from axios into cheerio
-    let $ = cheerio.load(response.data);
-    // For each element with a "title" class
-    $(".long-headline").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      let title = $(element).children("a").text();
-      let link = $(element).children("a").attr("href");
+// Making a request via axios for reddit's "webdev" board. We are sure to use old.reddit due to changes in HTML structure for the new reddit. The page's Response is passed as our promise argument.
+//axios.get("https://old.reddit.com/r/webdev").then(function(response) {
+  axios.get("https://www.clickhole.com/c/news").then(function(response) {
 
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
+    // Load the Response into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    var $ = cheerio.load(response.data);
+  
+    // An empty array to save the data that we'll scrape
+    var results = [];
+  
+    // With cheerio, find each p-tag with the "title" class
+    // (i: iterator. element: the current element)
+    $(".cw4lnv-5").each(function(i, element) {
+  
+      // Save the text of the element in a "title" variable
+      var title = $(element).text();
+  
+      // In the currently selected element, look at its child elements (i.e., its a-tags),
+      // then save the values for any "href" attributes that the child elements may have
+      var link = $(element).children().attr("href");
+  
+      // Save these results in an object that we'll push into the results array we defined earlier
+      results.push({
+        title: title,
+        link: link
+      });
     });
+  
+    // Log the results once you've looped through each of the elements found with cheerio
+    console.log(results);
   });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
 
 // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
